46396168f4e1aaf27bfdf2cbeb38b810874c59a0
denni <dennislandero40@gmail.com>
2024-03-18 22:27:09 -0600

    cambio en visualizacion del texto

--- app/src/main/java/com/example/tarea2p2/MainActivity.java
+++ app/src/main/java/com/example/tarea2p2/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +17,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BOTONES();
+
+        TextView textView3 = (TextView) findViewById(R.id.textView3);
+        textView3.setText("Integrantes del grupo: \n" +
+                "- Esdras Abiel Clother Paz (202010010048)\n" +
+                "- Eber Orestes González Garcia (201810030032)\n" +
+                "- Bayardo Antonio Ponce Ruiz (202110110125)\n" +
+                "- Jafeth Donatilo Rápalo Rivera (202120030040)\n" +
+                "- Dennis Antonio Landero Ramos (202120030040)\n" +
+                "- Delcer Oviel Hernandez (201510050050)");
 
     }
 
